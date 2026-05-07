8467e30000a9984d96103112ba893849dc5e3260
VIDHYASHREE N <vidhyashree2504@gmail.com>
2026-05-07 11:23:55 +0530

    Update app.py
    update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,10 @@
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-print("ML libraries loaded successfully")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "CodeVisionAI Backend Running Successfully"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
